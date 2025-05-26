abc6a37dc573f44ba79f167f6e23ffdd259e5b0f
Mrunmayee Pawar <153829829+mrunmayee299@users.noreply.github.com>
2025-05-26 21:08:59 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("hello, Myself Mrunmayee Pawar."
+        System.out.println("This is java app for Jenkins Pipeline carried out on :  " + currentDate);
+    }
+}
