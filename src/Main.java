16aa1cb28c186a302313b176a8a1e7655403fcbd
Mrunmayee Pawar <153829829+mrunmayee299@users.noreply.github.com>
2025-05-26 23:28:57 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
-        System.out.println("hello, Myself Mrunmayee Pawar.Intern at technohacks"
+        System.out.println("hello, Myself Mrunmayee Pawar,Interning at Technohacks Solutions Pvt Ltd as a DevOps Intern.Thanks for the Opportunity");
         System.out.println("This is java app for Jenkins Pipeline carried out on :  " + currentDate);
     }
 }
